20933e10066628698159b15835f73bdf43daa224
백대홍 <bdh3620@naver.com>
2024-06-17 23:32:49 +0900

    Feat:1장 시작

---
+++ 1.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+//MARK: - 변수(Variables)
+/*:
+![Constants Syntax](var.png)
+*/
+var greeting = "Hello! My name is DaeHong"
+greeting = "GoodBye! "
+print (greeting)
+
+
+//MARK: - 상수(Constants
+/*:
+![Constants Syntax](let.png)
+*/
+let Beverage = "Coffe"
+//Beverage = "Sprite"   //컴파일 에러!
+
+
+//MARK: - 기본 데이터 타입
+/*:
+![Constants Syntax](data.png)
+*/
+
+var someBool: Bool = true       //부울 타입
+
+var someInt: Int = 30           //정수 타입
+
+var someFloat: Float = 3.14     //실수 타입(32비트)
+
+var someDouble: Double = 3.14   //실수 타입(64비튼)
+
+var someCharacter: Character = "😍" //문자 타입
+
+var someString: String = "재밌는 Swift 문법시간..😄"
+
+someString = """
+여러줄 문자열은 이렇게
+해야만 합니다. 겹따옴표 3개 안에서
+벗어난곳에 입력하면 오류가 발생 합니다.
+"""
