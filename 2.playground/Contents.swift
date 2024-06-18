f121beab785e42c120c001062eedbf34def11c1a
백대홍 <bdh3620@naver.com>
2024-06-18 20:30:18 +0900

    Feat:2장 함수 끝

--- 2.playground/Contents.swift
+++ 2.playground/Contents.swift
@@ -1,3 +1,40 @@
 import UIKit
+//MARK: - 함수
+/*:
+![Constants Syntax](function.png)
+*/
 
-var greeting = "Hello, playground"
+/*
+func 함수이름(매개변수...) -> 반환 타입 {
+    /* 함수 구현 부분*/
+    return 반환 값
+}
+*/
+
+
+//함수 선언의 기본 형태
+func sum(a:Int, b: Int) -> Int {
+    return a + b
+}
+
+sum(a: 3, b: 5)
+print(sum(a: 31, b: 5))
+
+
+//변환 값이 없는 함수
+func printMyName(name: String) {
+    print(name)
+}
+
+printMyName(name: "백스터")
+
+
+//매개변수가 없는 함수
+func mosthighValue() -> Int {
+    return Int.max
+}
+
+mosthighValue()     //Int의 최댓값
+
+
+
