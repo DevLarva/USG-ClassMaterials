d4446ede2f882baa07a50bbf7b77cb6e64a73f0d
백대홍 <bdh3620@naver.com>
2024-06-18 21:56:57 +0900

    Feat:2장 조건문 끝

--- 2.playground/Contents.swift
+++ 2.playground/Contents.swift
@@ -4,7 +4,8 @@ import UIKit
 ![Constants Syntax](function.png)
 */
 
-/*
+
+/* 함수 기본 형태
 func 함수이름(매개변수...) -> 반환 타입 {
     /* 함수 구현 부분*/
     return 반환 값
@@ -36,5 +37,61 @@ func mosthighValue() -> Int {
 
 mosthighValue()     //Int의 최댓값
 
+
+//MARK: - 조건문
+/*:
+![Constants Syntax](conditional.png)
+*/
+
+/*:
+### if-else 구문
+*/
+
+
+/*
+ if 조건 {
+     /* 실행 구문 */
+ } else if 조건 {
+     /* 실행 구문 */
+ } else {
+     /* 실행 구문 */
+ }
+*/
+
+
+let someInt = 100
+
+if someInt < 100 {
+    print("100미만")
+} else if someInt > 100 {
+    print("100초과")
+} else {
+    print("100")
+}
+
+
+/*:
+### switch 구문
+*/
+
+/*
+switch 비교값 {
+case 패턴:
+    /* 실행 구문 */
+default:
+    /* 실행 구문 */
+}
+*/
+
+switch someInt {
+case 0:
+    print("zero")
+case 1..<100:
+    print("1~99")
+case 100:
+    print("100")
+default:
+    print("unknown")
+}
 
 
