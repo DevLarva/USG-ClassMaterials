06d3757a8820767ab4dfc5b70046b44dc817e1a8
백대홍 <bdh3620@naver.com>
2024-06-24 21:55:02 +0900

    Edit:1장 일부 수정

--- 1.playground/Contents.swift
+++ 1.playground/Contents.swift
@@ -4,9 +4,11 @@ import UIKit
 /*:
 ![Constants Syntax](var.png)
 */
-var greeting = "Hello! My name is DaeHong"
-greeting = "GoodBye! "
-print (greeting)
+var greeting = "Hello! My name is Baekster"
+greeting = "GoodBye!"
+print(greeting)
+
+//var grade    //annotation 에러
 
 
 //MARK: - 상수(Constants)
@@ -33,11 +35,12 @@ var someDouble: Double = 3.14   //실수 타입(64비튼)
 var someCharacter: Character = "😍" //문자 타입
 
 var someString: String = "재밌는 Swift 문법시간..😄"
-
 someString = """
 여러줄 문자열은 이렇게
-하면 됩니다. 겹따옴표 3개 안에서
+하면 됩니다. 큰따옴표 3개 안에서
 벗어난곳에 입력하면 오류가 발생 합니다.
 """
 
+
+//print("Int: \(Int.min) 부터 \(Int.max) 까지")
 
